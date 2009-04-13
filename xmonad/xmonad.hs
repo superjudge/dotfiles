340599a6f9ce46506767d5c59366dc45ec82d042
Johan Liseborn <mjl@mjl-laptop.(none)>
2009-04-13 21:00:49 +0200

    Added Xmonad configuration.

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,14 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+          xmonad $ defaultConfig
+                     { manageHook = manageDocks <+> manageHook defaultConfig
+                     , layoutHook = avoidStruts  $  layoutHook defaultConfig
+                     , modMask = mod1Mask
+                     , terminal = "urxvt"
+                     }
