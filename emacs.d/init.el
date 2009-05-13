269aa40d68d234d89b8a8a3ab70a27e6c1bea13c
Johan Liseborn <johan.liseborn@trioptima.com>
2009-05-13 09:10:45 +0200

    Minor tweaks.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -85,11 +85,11 @@
 (add-hook 'write-file-hook 'time-stamp)
 
 ;;; Auto-insert
-(require 'autoinsert)
-(auto-insert-mode)
-(setq auto-insert-directory "~/.emacs.d/templates/")
-(setq auto-insert-query nil)
-(define-auto-insert "\.py" "python.py")
+;; (require 'autoinsert)
+;; (auto-insert-mode)
+;; (setq auto-insert-directory "~/.emacs.d/templates/")
+;; (setq auto-insert-query nil)
+;; (define-auto-insert "\.py" "python.py")
 
 ;;; Calendar & Time stuff...
 (setq calendar-latitude 59.3)
