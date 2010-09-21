6e18b8a01212a5343eba5bf56161cb433f510b3e
Johan Liseborn <johan.liseborn@klarna.com>
2010-09-21 12:45:15 +0200

    Update copyright.

--- emacs.d/starter-kit-local.el
+++ emacs.d/starter-kit-local.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; coding: utf-8 -*-
 
-;; Copyright (c) 2009, Johan Liseborn <johan.liseborn@gmail.com>
+;; Copyright (c) 2009, 2010 Johan Liseborn <johan.liseborn@gmail.com>
 ;;
 ;; Permission to use, copy, modify, and/or distribute this software for any
 ;; purpose with or without fee is hereby granted, provided that the above
