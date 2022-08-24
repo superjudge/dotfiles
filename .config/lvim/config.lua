e4eefe7a068605a0854d6a69a875ccbba88b9563
Johan Liseborn <johan@castlerock.se>
2022-08-24 20:20:33 +0200

    Add some linters and formatters

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -152,26 +152,37 @@ formatters.setup {
   --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
   --     filetypes = { "typescript", "typescriptreact" },
   --   },
-  { exe = "goimports", filetypes = { "go" } }
+  { name = "goimports" },
+  { name = "beautysh" },
+  { command = "golines", extra_args = { "-m", "88" } },
+  { name = "clang_format", filetypes = { "proto" } },
+  { name = "jq" },
+  { command = "eslint_d" },
+  { command = "rustfmt" },
 }
 
 -- -- set additional linters
--- local linters = require "lvim.lsp.null-ls.linters"
--- linters.setup {
---   { command = "flake8", filetypes = { "python" } },
---   {
---     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "shellcheck",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--severity", "warning" },
---   },
---   {
---     command = "codespell",
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "javascript", "python" },
---   },
--- }
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  --   { command = "flake8", filetypes = { "python" } },
+  --   {
+  --     -- each linter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+  --     command = "shellcheck",
+  --     ---@usage arguments to pass to the formatter
+  --     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+  --     extra_args = { "--severity", "warning" },
+  --   },
+  --   {
+  --     command = "codespell",
+  --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+  --     filetypes = { "javascript", "python" },
+  --   },
+  { command = "buf" },
+  { name = "golangci_lint" },
+  { name = "shellcheck" },
+  { name = "zsh" },
+  { name = "proselint" },
+}
 
 -- Additional Plugins
 lvim.plugins = {
