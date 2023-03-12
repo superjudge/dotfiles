18e73fd1ef9a29e076951c49e5c6df15666f6d5e
Johan Liseborn <johan@castlerock.se>
2023-03-12 13:14:43 +0100

    Add Twilight

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -239,6 +239,16 @@ lvim.plugins = {
       }
     end
   },
+  {
+    "folke/twilight.nvim",
+    config = function()
+      require("twilight").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  },
   --     {"folke/tokyonight.nvim"},
   --     {
   --       "folke/trouble.nvim",
