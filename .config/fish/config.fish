24b55b25cdb4262fccf4e035c6b665b1539ae117
Johan Liseborn <johan@castlerock.se>
2023-08-18 19:45:01 +0200

    Clean up

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
+    if test -f /usr/share/doc/fzf/examples/key-bindings.fish
+      source /usr/share/doc/fzf/examples/key-bindings.fish
+      fzf_key_bindings
+    end
 
-    base16-gruvbox-dark-medium
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    if test -f "$BASE16_SHELL/profile_helper.fish"
+      source "$BASE16_SHELL/profile_helper.fish"
+      base16-gruvbox-dark-medium
+    end
 
     zoxide init fish | source
     starship init fish | source
