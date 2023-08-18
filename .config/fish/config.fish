4207f12f13c32ac71a54851540feffbccf301ea6
Johan Liseborn <johan@castlerock.se>
2023-08-18 19:40:31 +0200

    Fix color theme name

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ if status is-interactive
     set BASE16_SHELL "$HOME/.config/base16-shell/"
     source "$BASE16_SHELL/profile_helper.fish"
 
-    base16-gruvbox-dark-material-medium
+    base16-gruvbox-dark-medium
 
     zoxide init fish | source
     starship init fish | source
