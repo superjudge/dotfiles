03a02b75f7dbcd27d34eccdf5e60823edb5ea834
Johan Liseborn <johan@castlerock.se>
2023-08-18 19:13:09 +0200

    Add Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,8 @@ if status is-interactive
   set BASE16_SHELL "$HOME/.config/base16-shell/"
   source "$BASE16_SHELL/profile_helper.fish"
 
+  base16-gruvbox-dark-medium
+
+  zoxide init fish | source
   starship init fish | source
 end
-
-base16-gruvbox-dark-medium
